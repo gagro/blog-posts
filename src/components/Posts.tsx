ce94c9e5af61c6caad026b4c09558d4c30eaa3c0
gagro <toni.gagro@gmail.com>
2023-02-03 13:01:28 +0100

    Update Posts component and README.md file

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Post as PostType, User } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import sadImg from "../../public/sad.png";
+import Image from "next/image";
+import Link from "next/link";
 
 interface PostProps extends PostType {
   user: User;
@@ -27,14 +31,31 @@ const Post: React.FC<{ post: PostProps }> = ({ post }) => {
 };
 
 const Posts: React.FC<{ posts: PostProps[] }> = React.memo(({ posts }) => {
+  const { data: sessionData } = useSession();
+
   return (
-    <ul className="w-full">
-      {posts.map((post) => (
-        <li key={post.id}>
-          <Post post={post} />
-        </li>
-      ))}
-    </ul>
+    <>
+      {posts.length ? (
+        <ul className="w-full">
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Post post={post} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="flex items-center">
+          Looks like there are no posts.{" "}
+          <Image className="mx-2" src={sadImg} height={30} alt="Sad face" />{" "}
+          <Link
+            className="underline"
+            href={sessionData ? "/new-post" : "/login"}
+          >
+            You can change that.
+          </Link>
+        </p>
+      )}
+    </>
   );
 });
 
--- src/pages/my-posts.tsx
+++ src/pages/my-posts.tsx
@@ -2,25 +2,11 @@ import React from "react";
 import { NextPage } from "next";
 import { api } from "../utils/api";
 import Posts from "../components/Posts";
-import Link from "next/link";
 
 const MyPosts: NextPage = (props) => {
   const { data: posts } = api.post.fetchUserPosts.useQuery();
 
-  return (
-    <>
-      {posts?.length ? (
-        <Posts posts={posts} />
-      ) : (
-        <p className="text-center">
-          It's empty in here.{" "}
-          <Link className="underline" href="/new-post">
-            Please post something.
-          </Link>
-        </p>
-      )}
-    </>
-  );
+  return <Posts posts={posts || []} />;
 };
 
 export default MyPosts;
